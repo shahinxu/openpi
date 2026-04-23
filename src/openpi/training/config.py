c161e67c7aa8772a841b69ebca1f326dd6ad741c
shahinxu <1849197801@qq.com>
2026-04-23 15:25:45 +0200

    1

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -360,6 +360,38 @@ _CONFIGS = [
         ema_decay=None,
         wandb_enabled=True,
     ),
+    TrainConfig(
+        name="pi05_hannes_dataset_new",
+        model=pi0_config.Pi0Config(
+            action_dim=32,
+            action_horizon=10,
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+            pi05=True,
+            action_quantization_weight=0.1,
+        ),
+        data=HannesDataConfig(
+            repo_id="hannes/dataset_new",
+            assets=AssetsConfig(assets_dir="./assets/pi05_hannes_all"),
+            base_config=DataConfig(prompt_from_task=False),
+            extra_delta_transform=False,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "gs://openpi-assets/checkpoints/pi05_base/params"
+        ),
+        batch_size=32,
+        num_train_steps=30_000,
+        save_interval=1000,
+        freeze_filter=pi0_config.Pi0Config(
+            action_dim=32,
+            action_horizon=10,
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+            pi05=True,
+        ).get_freeze_filter(),
+        ema_decay=None,
+        wandb_enabled=True,
+    ),
 ]
 
 if len({config.name for config in _CONFIGS}) != len(_CONFIGS):
